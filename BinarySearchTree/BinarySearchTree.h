c5cc6851ebaa5507804ef8e8f733b5da5e155eb6
Raykevin-live <raykevin121@gmail.com>
2024-11-28 23:36:20 +0800

    二叉树

---
+++ BinarySearchTree/BinarySearchTree.h
@@ -0,0 +1,181 @@
+#pragma once
+using namespace std;
+
+template<class K>
+struct BSTreeNode
+{
+	BSTreeNode<K>* _left;
+	BSTreeNode<K>* _right;
+	K _key;
+	BSTreeNode(const K& key)
+		:_left(nullptr)
+		,_right(nullptr)
+		,_key(key)
+	{}
+};
+
+template<class K>
+class BSTree
+{
+	typedef BSTreeNode<K> Node;
+public:
+	BSTree()
+		:_root(nullptr)
+	{}
+
+	bool Insert(const K& key)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(key);
+			return true;
+		}
+
+		Node* cur = _root;
+		Node* parent = cur;
+
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		cur = new Node(key);
+		if (parent->_key < key)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+		return true;
+	}
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+		_InOrder(root->_left);
+		cout << root->_key <<' ';
+		_InOrder(root->_right);
+	}
+	void InOrder()
+	{
+		_InOrder(_root);
+		cout << endl;
+	}
+	bool Find(const K& key)
+	{
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				cur = cur->_left;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+	bool Erase(const K& key)
+	{
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				if (cur->_left == nullptr)
+				{
+					if (cur == _root)
+					{
+						_root = cur->_right;
+					}
+					if (parent->_left == cur)
+					{
+						parent->_left = cur->_right;
+					}
+					else
+					{
+						parent->_right = cur->_right;
+					}
+				}
+				else if (cur->_right == nullptr)
+				{
+					if (cur == _root)
+					{
+						_root = cur->_left;
+					}
+					if (parent->_left == cur)
+					{
+						parent->_left = cur->_left;
+					}
+					else
+					{
+						parent->_right = cur->_left;
+					}
+					return true; 
+				}
+				else
+				{
+					Node* leftMax = cur->_left;
+					Node* leftMaxParent = cur;
+					while (leftMax->_right)
+					{
+						leftMaxParent = cur;
+						leftMax = leftMax->_right;
+					}
+					std::swap(cur->_key, leftMax->_key);
+					//这地方可能不进去，在根上
+					if (leftMaxParent->_left == leftMax)
+					{
+						leftMaxParent->_left = leftMax->_left;
+					}
+					else
+					{
+						leftMaxParent->_right = leftMax->_left;
+					}
+					cur = leftMax;
+				}
+				delete cur;
+				return true;
+			}
+		}
+		return false;
+	}
+private:
+	Node* _root;
+};
+
+
--- BinarySearchTree/Test.cpp
+++ BinarySearchTree/Test.cpp
@@ -3,7 +3,41 @@
 #include "BinarySearchTree.h"
 using namespace std;
 
-class A 
+void TestBSTree1()
 {
+	int a[] = { 8, 3 ,1, 10, 6, 4, 7, 14, 13 };
+	BSTree<int> t;
+	for (auto e : a)
+	{
+		t.Insert(e);
+	}
+	t.InOrder();
+}
+
+void TestBSTree2()
+{
+	int a[] = { 8, 3 ,1, 10, 6, 4, 7, 14, 13 };
+	BSTree<int> t;
+	for (auto e : a)
+	{
+		t.Insert(e);
+	}
+	t.Erase(4);
+	t.InOrder();
 	
-};
+
+	t.Erase(6);
+	t.InOrder();
+
+	t.Erase(7);
+	t.InOrder();
+
+	t.Erase(7);
+	t.InOrder();
+}
+int main()
+{
+	TestBSTree2();
+
+	return 0;
+}
