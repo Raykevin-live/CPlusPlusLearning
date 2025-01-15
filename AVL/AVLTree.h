dd8d844e37ce570f8853e90da55d8b964efbe907
Raykevin-live <raykevin121@gmail.com>
2025-01-15 23:14:16 +0800

    AVL树插入

--- AVL/AVLTree.h
+++ AVL/AVLTree.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<iostream>
 #include<cassert>
+#include<vector>
 using namespace std;
 
 template<class K, class V>
@@ -89,9 +90,21 @@ public:
 			else if (parent->_bf == 2 || parent->_bf == -2)
 			{
 				// 子树不平衡需要旋转
-				if (parent->_bf == 2 || cur->bf == 1)
+				if (parent->_bf == 2 && cur->_bf == 1)
 				{
 					RotateL(parent);
+				}
+				else if (parent->_bf == -2 && cur->_bf == -1)
+				{
+					RotateR(parent);
+				}
+				else if (parent->_bf == 2 && cur->_bf == -1)
+				{
+					RotateRL(parent);
+				}
+				else if (parent->_bf == -2 && cur->_bf == 1)
+				{
+					RotateLR(parent);
 				}
 				break;
 			}
@@ -132,8 +145,135 @@ public:
 			{
 				ppnode->_right = cur;
 			}
-			parent->_bf = cur->_bf = 0;
+			cur->_parent = ppnode;
 		}
+		parent->_bf = cur->_bf = 0;
+
+	}
+	void RotateR(Node* parent)
+	{
+		Node* cur = parent->_left;
+		Node* curright = cur->_right;
+
+		parent->_left = curright;
+		if (curright)
+		{
+			curright->_parent = parent;
+		}
+		cur->_right = parent;
+		Node* ppnode = parent->_parent;
+		parent->_parent = cur;
+
+		if (parent == _root)
+		{
+			_root = cur;
+			cur->_parent = nullptr;
+		}
+		else
+		{
+			if (ppnode->_left == parent)
+			{
+				ppnode->_left = cur;
+			}
+			else
+			{
+				ppnode->_right = cur;
+			}
+			cur->_parent = ppnode;
+		}
+		//更新bf
+		parent->_bf = cur->_bf = 0;
+
+	}
+	void RotateRL(Node* parent)
+	{
+		Node* cur = parent->_right;
+		Node* curleft = cur->_left;
+		int bf = curleft->_bf;
+
+		RotateR(parent->_right);
+		RotateL(parent);
+		if (bf == 0)
+		{
+			cur->_bf = 0;
+			curleft->_bf = 0;
+			parent->_bf = 0;
+		}
+		else if (bf == 1)
+		{
+			cur->_bf = 0;
+			curleft->_bf = 0;
+			parent->_bf = -1;
+		}
+		else if (bf == -1)
+		{
+			cur->_bf = 1;
+			curleft->_bf = 0;
+			parent->_bf = 0;
+		}
+		else
+		{
+			assert(false);
+		}
+	}
+	void RotateLR(Node* parent)
+	{
+		Node* cur = parent->_left;
+		Node* curright = cur->_right;
+
+		int bf = curright->_bf;
+
+		RotateL(parent->_left);
+		RotateR(parent);
+
+		if (bf == 0)
+		{
+			cur->_bf = 0;
+			curright->_bf = 0;
+			parent->_bf = 0;
+		}
+		else if (bf == 1)
+		{
+			cur->_bf = -1;
+			curright->_bf = 0;
+			parent->_bf = 0;
+		}
+		else if (bf == -1)
+		{
+			cur->_bf = 0;
+			curright->_bf = 0;
+			parent->_bf = 1;
+		}
+		else
+		{
+			assert(false);
+		}
+	}
+	int Height(Node* root)
+	{
+		if (root == nullptr)
+			return 0;
+		int leftHeight = Height(root->_left);
+		int rightHeight = Height(root->_right);
+		return max(leftHeight, rightHeight) + 1;
+	}
+	bool IsBalance()
+	{
+		return IsBalance(_root);
+	}
+	bool IsBalance(Node* root)
+	{
+		if (root == nullptr) return true;
+
+		int leftHeight = Height(root->_left);
+		int rightHeight = Height(root->_right);
+		if (rightHeight - leftHeight != root->_bf) {
+			cout << "平衡因子异常" << root->_kv.first << " -> " << root->_bf << endl;
+			return false;
+		}
+		return abs(leftHeight - rightHeight) < 2
+			&& IsBalance(root->_left)
+			&& IsBalance(root->_right);
 	}
 private:
 	Node* _root = nullptr;
--- AVL/Test.cpp
+++ AVL/Test.cpp
@@ -3,6 +3,25 @@
 
 int main()
 {
+	//int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	//int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+	const int N = 1000000;
+	vector<int> v;
+	v.reserve(N);
+	srand(time(0));
+	for (size_t i = 0; i < N; i++)
+	{
+		v.push_back(rand());
+	}
+	AVLTree<int, int> t;
+	for (auto e : v)
+	{
+		t.Insert(make_pair(e, e));
+		/*if (e == 7) {
+			int x = 1;
+		}
+		cout << "Insert: " << e << "->" << t.IsBalance() << endl;*/
+	}
 
 
 	return 0;
